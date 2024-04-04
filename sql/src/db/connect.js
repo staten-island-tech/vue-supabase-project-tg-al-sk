8480faf8ac78b281c193750772456b71e80e0007
Samuel Kipnis <sam.kipnis@gmail.com>
2024-04-04 09:53:59 -0400

    db

---
+++ sql/src/db/connect.js
@@ -0,0 +1,4 @@
+import config from 'dotenv'
+config()
+
+const DB_LINK = process.env.DB_LINK
--- sql/src/teachers/Teacher.js
+++ sql/src/teachers/Teacher.js
@@ -7,6 +7,8 @@ export default class Teacher {
     this.image = image
     this.rarity = rarity
     this.stats = calculateStats(subject, rarity)
+    // Wow my code is so readable right
+    // This is the sum of all stats
     this.power = Math.round(Object.values(this.stats).reduce((acc, cur) => (acc += cur)))
   }
 }
