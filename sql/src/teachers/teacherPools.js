1ce287f057e3cb0ab1dc87d891f4d4074fb25094
haitang-blossoms <136626552+angelinali96@users.noreply.github.com>
2024-04-04 22:40:10 -0400

    create pools

--- sql/src/teachers/teacherPools.js
+++ sql/src/teachers/teacherPools.js
@@ -1,35 +1,28 @@
 import { teachers } from "./teachers.js";
 
-const pools = [
-    {
-        math: teachers.value.filter((teacher) => teacher.subject === 'math')
-    },
-    {
-        english: teachers.value.filter((teacher) => teacher.subject === 'english')
-    },
-    {
-        science: teachers.value.filter((teacher) => teacher.subject === 'science')
-    },
-    {
-        history: teachers.value.filter((teacher) => teacher.subject === 'history')
-    },
-    {
-        russian: teachers.value.filter((teacher) => teacher.subject === 'russian')
-    },
-    {
-        physed: teachers.value.filter((teacher) => teacher.subject === 'physed')
-    },
-    {
-        tech: teachers.value.filter((teacher) => teacher.subject === 'tech')
-    },
-    {
-        principal: teachers.value.filter((teacher) => teacher.subject === 'principal')
-    },
-    {
-        defaultstaff: teachers.value.filter((teacher) => teacher.subject === 'default')
-    }
+let pools = [
+    teachers.value.filter((teacher) => teacher.subject === 'math'),
+    teachers.value.filter((teacher) => teacher.subject === 'english'),
+    teachers.value.filter((teacher) => teacher.subject === 'science'),
+    teachers.value.filter((teacher) => teacher.subject === 'history'),
+    teachers.value.filter((teacher) => teacher.subject === 'russian'),
+    teachers.value.filter((teacher) => teacher.subject === 'physed'),
+    teachers.value.filter((teacher) => teacher.subject === 'tech'),
+    teachers.value.filter((teacher) => teacher.subject === 'principal'),
+    teachers.value.filter((teacher) => teacher.subject === 'default'),
+    teachers.value.filter((teacher) => teacher.name === 'Michael Whalen')
 ];
 
-// push shits
-
+let student = {
+                subject: "average",
+                name: "student",
+                role: "average student",
+                image: "https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/5CKBK3QQKQI6TDYMN6DYUJRIRI.jpg"
+        }
+pools.forEach(function(item){
+    for(let i=0; i<60; i++){
+        item.push(student);
+    }
+})
+console.log(pools);
 export {pools};
