ab6a2f52e71e1d013701e2d6cbdbf75f4d604d5d
haitang-blossoms <136626552+angelinali96@users.noreply.github.com>
2024-04-18 10:16:27 -0400

    merge

--- sql/src/lib/ifNotSignedInGoToPage.js
+++ sql/src/lib/ifNotSignedInGoToPage.js
@@ -1,17 +1,5 @@
 import router from '../router/index'
 import isSignedIn from '../../db/auth/isSignedIn'
-import { useCounterStore } from '../stores/counter';
-
-export default function ifNotSignedInGoToPage() {
-    isSignedIn().then((signedIn) => {
-        if(signedIn) {
-            useCounterStore.loggedin.value = true;
-            console.log(useCounterStore.loggedin.value, 'logged in');
-        } else {
-            console.log('not logged in')
-            router.push('/login')
-        }
-    })
 
 export default function ifNotSignedInGoToPage(route) {
     if(route.path !== '/login' && route.path !== '/signup') {
