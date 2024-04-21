dd6bfc9793bbafd2c8a9785e19d28f11b8ae2e82
haitang-blossoms <136626552+angelinali96@users.noreply.github.com>
2024-04-21 12:04:11 -0400

    Update HomeView.vue
    ( ͡° ͜ʖ ͡°) i forgot to make the colors be consistent

--- sql/src/views/HomeView.vue
+++ sql/src/views/HomeView.vue
@@ -13,42 +13,42 @@
     <div class="grid">
         <div class="col-12 md:col-4 mb-4 px-5">
             <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                <i class="pi pi-user-edit text-4xl text-blue-500"></i>
+                <i class="pi pi-user-edit text-4xl text-green-500"></i>
             </span>
             <div class="text-900 text-xl mb-3 font-medium">Log In</div>
             <span class="text-700 line-height-3">Log out or create account. </span>
         </div>
         <div class="col-12 md:col-4 mb-4 px-5">
             <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                <i class="pi pi-book text-4xl text-blue-500"></i>
+                <i class="pi pi-book text-4xl text-green-500"></i>
             </span>
             <div class="text-900 text-xl mb-3 font-medium">View Available Cards</div>
             <span class="text-700 line-height-3">Preview all cards you can pull for. </span>
         </div>
         <div class="col-12 md:col-4 mb-4 px-5">
             <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                <i class="pi pi-calculator text-4xl text-blue-500"></i>
+                <i class="pi pi-calculator text-4xl text-green-500"></i>
             </span>
             <div class="text-900 text-xl mb-3 font-medium">Math for Golden Seagulls</div>
             <span class="text-700 line-height-3">Put your math skills to the test and earn currency for gacha pulls. </span>
         </div>
         <div class="col-12 md:col-4 mb-4 px-5">
             <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                <i class="pi pi-hammer text-4xl text-blue-500"></i>
+                <i class="pi pi-hammer text-4xl text-green-500"></i>
             </span>
             <div class="text-900 text-xl mb-3 font-medium">Battle for Golden Seagulls</div>
             <span class="text-700 line-height-3">Not trying to do math? Fight for your gacha pulls then. </span>
         </div>
         <div class="col-12 md:col-4 mb-4 px-5">
             <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                <i class="pi pi-money-bill text-4xl text-blue-500"></i>
+                <i class="pi pi-money-bill text-4xl text-green-500"></i>
             </span>
             <div class="text-900 text-xl mb-3 font-medium">Gacha</div>
             <span class="text-700 line-height-3">Wanna pull? Go to this page >:)</span>
         </div>
         <div class="col-12 md:col-4 md:mb-4 mb-0 px-3">
             <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                <i class="pi pi-trophy text-4xl text-blue-500"></i>
+                <i class="pi pi-trophy text-4xl text-green-500"></i>
             </span>
             <div class="text-900 text-xl mb-3 font-medium">Inventory</div>
             <span class="text-700 line-height-3">Check your obtained cards here. </span>
