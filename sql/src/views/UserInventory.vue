8e5063b88e28ccf5aa63a7f8d89dc020e0c3e37c
Samuel Kipnis <sam.kipnis@gmail.com>
2024-05-10 10:13:53 -0400

    e

--- sql/src/views/UserInventory.vue
+++ sql/src/views/UserInventory.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
 
-    </div>
 </template>
 
 <script setup lang="ts">
+import getCurrentUser from '@/db/getCurrentUser'
+import getUserByID from '@/db/getUserByID'
 
+const userAuth = await getCurrentUser()
+// console.log(userAuth.id)
+
+const user = getUserByID(userAuth.id)
+console.log(JSON.parse(JSON.stringify(user)))
 </script>
 
 <style scoped>
