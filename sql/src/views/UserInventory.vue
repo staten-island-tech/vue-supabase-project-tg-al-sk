03273f96774f21a7e11edf48fa20876b50a2b346
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-05-26 16:55:09 -0400

    teehee

--- sql/src/views/UserInventory.vue
+++ sql/src/views/UserInventory.vue
@@ -12,27 +12,24 @@
             <div v-for="(item, index) in slotProps.items" :key="index" class="col-12">
                 <div class="flex flex-column sm:flex-row sm:align-items-center p-4 gap-3" :class="{ 'border-top-1 surface-border': index !== 0 }">
                     <div class="md:w-10rem relative">
-                        <img class="block xl:block mx-auto border-round w-full" :src="`${item.image}`" :alt="item.name" />
+                        <img class="block xl:block mx-auto border-round w-full" :src="item.image" :alt="item.name" />
                         <Tag :value="item.inventoryStatus" class="absolute" style="left: 4px; top: 4px"></Tag>
                     </div>
                     <div class="flex flex-column md:flex-row justify-content-between md:align-items-center flex-1 gap-4">
                         <div class="flex flex-row md:flex-column justify-content-between align-items-start gap-2">
                             <div>
-                                <span class="font-medium text-secondary text-sm">{{ item.category }}</span>
                                 <div class="text-lg font-medium text-900 mt-2">{{ item.name }}</div>
                             </div>
                             <div class="surface-100 p-1" style="border-radius: 30px">
                                 <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
-                                    <span class="text-900 font-medium text-sm">{{ item.rating }}</span>
+                                    <span class="text-900 font-medium text-sm">{{ item.star }}</span>
                                     <i class="pi pi-star-fill text-yellow-500"></i>
                                 </div>
                             </div>
                         </div>
                         <div class="flex flex-column md:align-items-end gap-5">
-                            <span class="text-xl font-semibold text-900">${{ item.price }}</span>
                             <div class="flex flex-row-reverse md:flex-row gap-2">
-                                <Button icon="pi pi-heart" outlined></Button>
-                                <Button icon="pi pi-shopping-cart" label="Buy Now" :disabled="item.inventoryStatus === 'OUTOFSTOCK'" class="flex-auto md:flex-initial white-space-nowrap"></Button>
+                                <Button icon="pi pi-shopping-cart" label="Open Stats"  class="flex-auto white-space-nowrap"></Button>
                             </div>
                         </div>
                     </div>
@@ -47,14 +44,13 @@
                 <div class="p-4 border-1 surface-border surface-card border-round flex flex-column">
                     <div class="surface-50 flex justify-content-center border-round p-3">
                         <div class="relative mx-auto">
-                            <img class="border-round w-full" :src="`https://primefaces.org/cdn/primevue/images/product/${item.image}`" :alt="item.name" style="max-width: 300px"/>
+                            <img class="border-round w-full" :src="item.image" :alt="item.name" style="max-width: 300px"/>
                             <Tag :value="item.inventoryStatus" class="absolute" style="left: 4px; top: 4px"></Tag>
                         </div>
                     </div>
                     <div class="pt-4">
                         <div class="flex flex-row justify-content-between align-items-start gap-2">
                             <div>
-                                <span class="font-medium text-secondary text-sm">{{ item.category }}</span>
                                 <div class="text-lg font-medium text-900 mt-1">{{ item.name }}</div>
                             </div>
                             <div class="surface-100 p-1" style="border-radius: 30px">
@@ -65,10 +61,8 @@
                             </div>
                         </div>
                         <div class="flex flex-column gap-4 mt-4">
-                            <span class="text-2xl font-semibold text-900">${{ item.price }}</span>
                             <div class="flex gap-2">
                                 <Button icon="pi pi-shopping-cart" label="Open Stats"  class="flex-auto white-space-nowrap"></Button>
-                                <Button icon="pi pi-heart" outlined></Button>
                             </div>
                         </div>
                     </div>
@@ -91,22 +85,26 @@ import type { Ref } from 'vue'
 const layout: Ref<'grid'|'list'> = ref('grid');
 
 // @ts-ignore
-import getGacha from '/db/gacha/getGacha.js'
+//import getGacha from '/db/gacha/getGacha.js'
 
 //import { pools } from '@/teachers/teacherPools.ts';
 
 onMounted(()=> {
-    doSmth()
+    
 });
 
 
-async function doSmth() {
-    const userInventory = await getGacha();
-    if (userInventory.gacha > 0) {
-    console.log('hello i work????')
-    console.log(userInventory.gacha)
-}
-};
+import getCurrentUser from '@/db/getCurrentUser'
+import getGacha from '@/db/gacha/getGacha'
+
+const id = await (await getCurrentUser()).id
+console.log(id)
+const gacha = await getGacha(id)
+console.log(gacha) 
+import { useUserStore } from '@/db/pinia/stores/userStore'
+const userStore = useUserStore()
+console.log(userStore.getUser)
+
 
 const products = [
     {
@@ -124,18 +122,6 @@ const products = [
         "image": "https://3.files.edl.io/28a0/20/03/12/122230-d2392079-05d5-4940-a838-0d6146427076.jpg"
     },
 ];
-
-
-/* import getCurrentUser from '@/db/getCurrentUser'
-import getGacha from '@/db/gacha/getGacha'
-
-const id = await (await getCurrentUser()).id
-console.log(id)
-const gacha = await getGacha(id)
-console.log(gacha) */
-import { useUserStore } from '@/db/pinia/stores/userStore'
-const userStore = useUserStore()
-console.log(userStore.getUser)
 
 </script>
 
